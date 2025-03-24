8120178d3c42d905cc4410c5e494cb73913d5ba2
Arun <akcgc21@gmail.com>
2025-03-24 13:13:41 +0100

    make simple changes

--- clock/views.py
+++ clock/views.py
@@ -3,7 +3,12 @@ import pytz
 from datetime import datetime
 
 def world_clock(request):
-    timezones = ['UTC', 'America/New_York', 'Europe/London', 'Asia/Tokyo', 'Australia/Sydney']
-    world_times = {tz: datetime.now(pytz.timezone(tz)).strftime('%Y-%m-%d %H:%M:%S') for tz in timezones}
-    
-    return render(request, 'clock/world_clock.html', {'world_times': world_times})
+    all_timezones = pytz.all_timezones  # Get ALL time zones
+    selected_timezone = request.GET.get('timezone', 'UTC')  # Get user-selected timezone
+
+    world_times = {
+        tz: datetime.now(pytz.timezone(tz)).strftime('%Y-%m-%d %H:%M:%S')
+        for tz in ['UTC', 'America/New_York', 'Europe/London', 'Asia/Tokyo', 'Australia/Sydney', selected_timezone]
+    }
+
+    return render(request, 'clock/world_clock.html', {'world_times': world_times, 'all_timezones': all_timezones})
